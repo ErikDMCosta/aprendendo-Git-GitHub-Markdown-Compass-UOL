e83ed331264657ad03dc265aa4d56e0a4a8a91e5
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-01-26 19:41:28 -0300

    Criado a opção de ter capa de fundo e trocar

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,22 +1,26 @@
 <template>
         <div>
                 <img :src="avatar" :alt="descricao">
+                <img :src="capa" :alt="descricao">
                 <MudarImagem @mudar-imagem="trocarImagem" />
         </div>
 </template>
 
 <script>
     import Reutilizacao from './Reutilizacao.vue';
-    import MudarImagem from './MudarImagem.vue';
+    import MudarImagem1 from './MudarImagem.vue';
+    import MudarImagem2 from './MudarImagem.vue';
 
 export default {
         name:'Picture', 
         components: {
-              MudarImagem
+              MudarImagem1,
+              MudarImagem2
         },
         data() {
                 return {
                         avatar: "/img/avatar.png",
+                        capa: "/img/capa.png",
                         descricao: "Érik DM Costa"
                 }
         },
@@ -24,6 +28,7 @@ export default {
                 trocarImagem() {
                         //console.log("Opa");
                         this.avatar = "./img/avatar2.png";
+                        this.capa = "./img/capa2.png";
                 }
         }
 }
